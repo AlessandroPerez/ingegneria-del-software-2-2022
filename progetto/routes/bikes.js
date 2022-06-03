dc193091f0a38bde2ffa5dda206f460b5c6011be
AlessandroPerez <alessandro@BigJack.localdomain>
2022-06-03 15:48:22 +0200

    add reservations

--- progetto/routes/bikes.js
+++ progetto/routes/bikes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const {Bike, ReviewModel} = require('../model/Bike');
 
+const Reservations = require('../model/Reservations');
+
+const mongoose = require('mongoose');
 
 // Gets back all the bikes
 
@@ -55,7 +58,7 @@ router.delete('/:bikeId', async (req, res) => {
 });
 
 // Update a specific bike
-/*
+
 router.patch('/:bikeId', async (req, res) => {
   try {
     const updatedBike = await Bike.updateOne({_id:req.params.bikeId},
@@ -65,9 +68,9 @@ router.patch('/:bikeId', async (req, res) => {
     res.json({ message : err });
   }
 });
+
 // Make a reviw
 
-*/
 router.post('/:bikeId/review', async (req, res) => {
   try {
     const review = new ReviewModel({
@@ -83,6 +86,25 @@ router.post('/:bikeId/review', async (req, res) => {
     }
   } catch (err) {
     res.json ({message: err});
+  }
+});
+
+router.post('/:bikeId/reservation', async (req, res) => {
+  try {
+    const reservations = await Reservations.find({start_date: {$lt: req.body.end_date}, end_date: {$gt: req.body.start_date}, bike: mongoose.Types.ObjectId(req.params.bikeId)})
+    if (reservations?.length) {
+      res.json({message: "The Ebike is currently reserved"});
+    } else {
+      const reservation = new Reservations ({
+        start_date: req.body.start_date,
+        end_date: req.body.end_date,
+        bike: mongoose.Types.ObjectId(req.params.bikeId),
+      })
+      const savedReservation = await reservation.save();
+      res.json(savedReservation);
+    }
+  } catch (err) {
+    res.json({message: err});
   }
 });
 
